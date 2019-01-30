cb00f7f740435efc024de75a9e308e1f7cc6de62
Chancy <chancyleath@hotmail.com>
2019-01-30 10:06:11 -0700

    added to skills section and reformatted

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
-    //initialize modals
-    $(".modal").modal();
-    //initialize sidenav
-    $("#mobileNav").sidenav();
+$(document).ready(function () {
+  //initialize modals
+  $(".modal").modal();
+  //initialize sidenav
+  $("#mobileNav").sidenav();
 
-    //swiper and its customization
-    var mySwiper = new Swiper (".swiper-container", {
-        // Optional parameters
-        loop: true,
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-        delay: 2500,
-        disableOnInteraction: true,
-      },
-        effect: "flip",
-        grabCursor: true,
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-      });
+  //swiper and its customization
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+    },
+    effect: "flip",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 
